f64a10af2e34c4311d600e13e7590be485bbb90d
Ronni Kahalani <ronni.kahalani@gmail.com>
2022-10-17 09:52:02 +0200

    file feature updates

--- file/file.js
+++ file/file.js
@@ -32,9 +32,11 @@ class FileHandler {
      * Base64 encode a file.
      */
     async base64(file, callback) {
-        this.log(`Encoding file: ${file.name}...`)
+        this.log(`Encoding: ${file.name}...`)
+        let startTime = window.performance.now()
         let data = await this.convertBase64(file);
-        this.log(`Done encoding file: ${file.name}...`)
+        let endTime = window.performance.now()
+        this.log(`Encode completed (${(endTime - startTime).toFixed(2)} ms) for: ${file.name}.`)
         callback(data)
     }
 
@@ -45,12 +47,12 @@ class FileHandler {
         let element = document.getElementById(id)
         element.focus()
 
-        let data = element.value 
+        let data = element.value
 
         try {
             await navigator.clipboard.writeText(data)
             this.log('Copied to the clipboard successfully.')
-        }catch(error) {
+        } catch (error) {
             this.log(error)
         }
     }
@@ -59,57 +61,61 @@ class FileHandler {
      * Logs the message to the message div, on the web page.
      */
     log(message) {
-        $('#message').prepend(`<div>${message}</div>`)
+        $('#message').prepend(`<div class="row message-line">${new Date().toLocaleString() + ": " + message}</div>`)
+    }
+
+    handleFiles(files) {
+        // Clear the files element.
+        $('#files').text('')
+
+        // Go through all the files and encode them.
+        for (let index = 0; index < files.length; index++) {
+            let file = files[index]
+
+            fileHandler.base64(file, (data) => {
+                let idValue = 'file-' + index
+                // Append the current file encoded string.
+                let diffSize = (parseFloat(data.length - file.size))
+                let diffPct = (parseFloat((diffSize / file.size) * 100))
+
+                $('#files').append(
+                    `<div class="row file-row">
+
+                <div class="col">
+                    <img class="rounded" style="width:200px;height:150px;" src="${data}" />
+                </div>
+                
+                <div class="col">
+                    <div class="row">${file.name}</div>
+                    <div class="row">Type: ${file.type}</div>
+                    <div class="row">File size: ${file.size.toLocaleString()}</div>
+                    <div class="row">Text size: ${data.length.toLocaleString()}</div>
+                    <div class="row">Diff. size: ${diffSize.toLocaleString()} (${diffPct.toFixed(2).toLocaleString()}%)</div>
+                </div>
+
+                <div class="col-6">
+                    <textarea id="${idValue}" class="form-control col-xs-12" style="height:150px;">${data}</textarea>
+                </div>
+
+                <div class="col-1">
+                    <button class="btn btn-sm btn-primary" onclick="fileHandler.copyToClipboard('${idValue}')">Copy</button>
+                </div>
+            </div>`)
+
+                document.getElementById('fileCount').innerHTML = files.length
+                //console.log(data)
+            });
+        }
     }
 }
+var fileHandler = new FileHandler()
 
 /**
  * Event Hooks.
  */
 document.getElementById('image').addEventListener("change", (event) => {
-    // Get the list of files, from the file input element.
-    let files = event.target.files
-    // Get a file handler.
-    let fileHandler = new FileHandler()
-    // Clear the files element.
-    $('#files').text('')
-
-    // Go through all the files and encode them.
-    for (let index = 0; index < files.length; index++) {
-        let file = event.target.files[index]
-
-        fileHandler.base64(file, (data) => {
-            let idValue = 'file-' + index
-            // Append the current file encoded string.
-            let diffSize = (parseFloat(data.length - file.size))
-            let diffPct = (parseFloat((diffSize / file.size) * 100))
-
-            $('#files').append(
-                `<div class="row">
-                    <div class="col-2">
-                        <img class="rounded" style="width:200px;height:150px;" src="${data}" />
-                    </div>
-                    <div class="col">
-                        <textarea id="${idValue}" class="form-control col-xs-12" style="height:150px;">${data}</textarea>
-                    </div>
-                    <div class="col-1">
-                        <button class="btn btn-sm btn-primary" onclick="fileHandler.copyToClipboard('${idValue}')">Copy</button>
-                    </div>
-                    <div class="col-2">
-                        <div class="row">${file.name}</div>
-                        <div class="row">File type: ${file.type}</div>
-                        <div class="row">File size: ${file.size.toLocaleString()}</div>
-                        <div class="row">Text size: ${data.length.toLocaleString()}</div>
-                        <div class="row">Diff. size: ${diffSize.toLocaleString()} (${diffPct.toFixed(2).toLocaleString()}%)</div>
-                    </div>
-                </div>`)
-                
-            //console.log(data)
-        });
-    }
+    fileHandler.handleFiles(event.target.files)
 });
-var fileHandler = new FileHandler()
-
 
 window.addEventListener('load', (event) => {
     $('.dropify').dropify();
