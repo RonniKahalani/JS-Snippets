c1056d2ce2f04126390e96efc366dbfd01bb5b54
Ronni Kahalani <ronni.kahalani@gmail.com>
2022-10-25 10:03:32 +0200

    updated exercise info

---
+++ template/README.md
@@ -0,0 +1,14 @@
+# JS-Snippets - template
+Shows how to use HTML templates.
+
+## Scenario
+You need a feature to minimize the HTML code in your JavaScript files, by using embedded and external template files.
+
+## Learning
+- Cloning templates, from the HTML file and from external HTML files
+- Get and read data to the template clone,
+- Adding the template clone to the current HTML page.
+
+## Exercise
+Make a web page that uses a HTML embedded and external template.
+- Use setAttribute/getAttribute to set/get template data.
---
+++ template/template.js
@@ -0,0 +1,46 @@
+'use strict'
+/**
+ * Handles calling REST API endpoints
+ */
+class TemplateHandler {
+
+    constructor() {
+
+    }
+
+    /**
+     * Load template from document.
+     */
+    loadFromDocument(templateId, targetId) {
+        let template = document.querySelector(templateId)
+        let clone = template.content.cloneNode(true)
+        let root = clone.querySelector("#embeddedRoot")
+        root.setAttribute("style", "background-color: green;color:white;")
+        root.textContent = "Loaded from the document."
+        let target = document.querySelector(targetId)
+        target.appendChild(clone)
+    }
+
+     /**
+     * Load template from file.
+     */
+      async loadFromFile(filePath, templateId, targetId) {
+
+        // Loading the template file, using jQuery for simplicity.
+            $('#fileTemplate').load("template.html", () => {
+
+                // Waiting for the data to be loaded, and now its ready.
+
+                let template = document.querySelector(templateId)
+                let clone = template.content.cloneNode(true)
+                let root = clone.querySelector("#externalRoot")
+                root.setAttribute("style", "background-color: blue;color:white;")
+                root.textContent = "Loaded from an external template file."
+                let target = document.querySelector(targetId)
+                target.appendChild(clone)
+            })
+
+        
+      
+        }
+}
